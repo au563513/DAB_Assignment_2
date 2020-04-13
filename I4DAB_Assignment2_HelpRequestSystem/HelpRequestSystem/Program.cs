395ef55fdbea1541a7b53eb25590bc8da0999805
Anders Tøgersen <54847232+au563513@users.noreply.github.com>
2020-04-13 11:08:48 +0200

    Reverted

--- I4DAB_Assignment2_HelpRequestSystem/HelpRequestSystem/Program.cs
+++ I4DAB_Assignment2_HelpRequestSystem/HelpRequestSystem/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Linq;
+
 using HelpRequestSystem.Services;
 using HelpRequestSystem.Data;
 using HelpRequestSystem.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace HelpRequestSystem
 {
@@ -11,46 +10,17 @@ namespace HelpRequestSystem
     {
         static void Main(string[] args)
         {
-            ClearDatabase();
-            SeedDatabase();
-
             using (var c = new HrsContext())
             {
-                var list = c.Students.AsNoTracking().ToList();
-                
-                foreach (var student in list)
-                {
-                    Console.WriteLine($"au{student.StudentId}, {student.StudentName}");
-                }
-            }
+                Student student = new Student();
+                student.StudentId = 563513;
+                student.StudentName = "Anders Tøgersen";
 
-        }
+                c.Students.Add(student);
 
-        public static void ClearDatabase()
-        {
-            using (var c = new HrsContext())
-            {
-                c.Database.EnsureDeleted();
-                c.Database.Migrate();
-            }
-        }
-
-        public static void SeedDatabase()
-        {
-            using (var c = new HrsContext())
-            {
-                c.Students.Add(CreateStudent(123456,"Anders"));
-                c.Students.Add(CreateStudent(123455,"Peter"));
-                c.Students.Add(CreateStudent(132213,"Julie"));
-                c.Students.Add(CreateStudent(634643,"Valdemar"));
-                c.Students.Add(CreateStudent(233253,"Jonas"));
                 c.SaveChanges();
             }
-        }
-
-        private static Student CreateStudent(int id, string name)
-        {
-            return new Student(){StudentId = id, StudentName = name};
+            
         }
     }
 }
